5360ad14093de491c26be3ce1be74dd17eb817ad
birdalguneyy <birdalguney.demirok@std.yeditepe.edu.tr>
2025-01-02 23:17:37 +0300

    I added Theme.kt & Color.kt

--- app/src/main/java/com/example/fitweather/ui/theme/Color.kt
+++ app/src/main/java/com/example/fitweather/ui/theme/Color.kt
@@ -1,11 +1,14 @@
 package com.example.fitweather.ui.theme
 
 import androidx.compose.ui.graphics.Color
-
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+//Colors App Colors//
+//-----------------------------------------------//
+//main orange color
+val colorOrange = Color(0xFFFB8C00)
+//orange color combo
+val colorOrangeCombo = Color(0xFFFFB74D)
+//color Black
+val colorBlack = Color(0xFF000000)
+//color White
+val colorWhite = Color(0xFFFFFFFF)
+//---------------------------------------------//
