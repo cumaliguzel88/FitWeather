ace9fa3666ee5bdf3d1d1f60ef2ba47d792934ec
Cumali Guzel <cumali.guzel@std.yeditepe.edu.tr>
2025-01-02 21:20:44 +0300

    Add the project dependencies and lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.fitweather"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.fitweather"
@@ -66,4 +66,26 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    //coil for picture
+    implementation("io.coil-kt.coil3:coil-compose:3.0.4")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")
+    //retrofit ve live data
+    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.6")
+    implementation ("androidx.compose.material:material-icons-extended:1.5.1")
+    //firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation("com.google.firebase:firebase-firestore:25.1.1")
+    implementation ("com.google.firebase:firebase-storage:21.0.1")
+    //pref
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    //navigtion
+    implementation("androidx.navigation:navigation-compose:2.8.5")
+    //lottie dependencies
+    implementation( "com.airbnb.android:lottie-compose:5.2.0")
+    //auth
+    implementation("com.google.firebase:firebase-auth")
+
+
 }
